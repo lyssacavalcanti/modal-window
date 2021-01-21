5dbcc93acda9e74128915c88c119578647a8bee7
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-21 11:20:55 -0300

    Initial Javavascript Set

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use sctrict'
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
