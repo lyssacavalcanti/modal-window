12eb82cead37a9658a95fbd0386ccb063501d231
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-24 17:31:00 -0300

    Add addEventListener in overlay class.

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ const btnsCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', function() {
+        console.log('Button clicked')
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden');
+    });
+
+    btnsCloseModal.addEventListener('click', function() {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+
+    overlay.addEventListener('click', function () {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+
+    
+
+ 
