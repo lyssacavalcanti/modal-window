30316cf1b34bfc2c40313b0be35e685c7dec83cb
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-24 17:54:57 -0300

    Refactoring some code blocks

--- script.js
+++ script.js
@@ -4,26 +4,17 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-
-console.log(btnsOpenModal);
+const openModal = function() {
+    console.log('Button clicked')
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden');
+};
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', function() {
-        console.log('Button clicked')
-        modal.classList.remove('hidden')
-        overlay.classList.remove('hidden');
-    });
-
-    btnsCloseModal.addEventListener('click', function() {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
-
-    overlay.addEventListener('click', function () {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
-
-    
-
- 
+    btnsOpenModal[i].addEventListener('click', openModal);
+    btnsCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
